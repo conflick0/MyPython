0d265a7c03764aca5877fa1162d5540fbd9c6ab7
conflick0 <jpjhsu@gms.tku.edu.tw>
2019-05-30 14:46:26 +0800

    add img

--- CarRecognize/recognize.py
+++ CarRecognize/recognize.py
@@ -1,57 +1,84 @@
 import cv2
 import requests
-
-# Replace <Subscription Key> with your valid subscription key.
-subscription_key = ""
-assert subscription_key
+import time
 
 
-vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+def get_license(img_name,image_url):
+    # Replace <Subscription Key> with your valid subscription key.
+    subscription_key = "f5fe0534bb63445585769f1a3e1b7f94"
+    assert subscription_key
 
-ocr_url = vision_base_url + "ocr"
+    vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
 
-# Set image_url to the URL of an image that you want to analyze.
-image_url = "https://www.cardu.com.tw/image_upload/news/28/U20151119083338.jpg"
-#"https://img.incar.tw/files/styles/media_gallery_large/public/media/i/m/g/0/7/1/8/img_0718_2.jpg" #car3
+    text_recognition_url = vision_base_url + "read/core/asyncBatchAnalyze"
 
-headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-params  = {'language': 'unk', 'detectOrientation': 'true'}
-data    = {'url': image_url}
-response = requests.post(ocr_url, headers=headers, params=params, json=data)
-response.raise_for_status()
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+    params = {'mode': 'Handwritten'}
+    data = {'url': image_url}
+    response = requests.post(
+        text_recognition_url, headers=headers, params=params, json=data)
+    response.raise_for_status()
 
-analysis = response.json()
+    # The recognized text isn't immediately available, so poll to wait for completion.
+    analysis = {}
+    poll = True
+    while (poll):
+        response_final = requests.get(
+            response.headers["Operation-Location"], headers=headers)
+        analysis = response_final.json()
+        print(analysis)
+        time.sleep(1)
+        if ("recognitionResults" in analysis):
+            poll = False
+        if ("status" in analysis and analysis['status'] == 'Failed'):
+            poll = False
 
-line_infos = [region["lines"] for region in analysis["regions"]]
-word_infos = []
-for line in line_infos:
-    for word_metadata in line:
-        for word_info in word_metadata["words"]:
-            word_infos.append(word_info)
-
-print(word_infos)
-
-box_list = []
-for word in word_infos:
-    pos = []
-    bound = word["boundingBox"].split(',')  # ['304', '304', '88', '29']
-    for b in bound:
-        pos.append(int(b))
-    text = word["text"]
-    box_list.append([pos, text])
-
-
-img = cv2.imread('./img/car2.jpg')
-for box in box_list:
-    cv2.rectangle(img, (box[0][0], box[0][1]), (box[0][0] + box[0][2], box[0][1] + box[0][3]), (0, 255, 0), 2)
-    cv2.putText(img, box[1], (box[0][0] - 20, box[0][1] - 20), 0, 1, (0, 255, 0), 2)
+    polygons = []
+    if ("recognitionResults" in analysis):
+        # Extract the recognized text, with bounding boxes.
+        polygons = [(line["boundingBox"], line["text"])
+                    for line in analysis["recognitionResults"][0]["lines"]]
+    print(polygons)
+    show_img(polygons, img_name)
 
 
-cv2.imshow('My Image', img)
+def show_img(polygons, img_name):
+    img = cv2.imread('./img/'+img_name+'.jpg')
+    for box in polygons:
+        cv2.rectangle(img, (box[0][0], box[0][1]), (box[0][4], box[0][5]), (0, 255, 0), 2)
+        cv2.putText(img, box[1], (box[0][0] - 20, box[0][1] - 20), 0, 1, (0, 255, 0), 2)
 
-# press any key to close the window
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow(img_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
+def downloadImg(name, url):
+    folder_path = r'./img/' + name + '.jpg'
+
+    try:
+        html = requests.get(url)  # use 'get' to get photo link path , requests = send request
+    except Exception as e:
+        print('[!]Error : ' + str(e))
+    else:
+        img_name = folder_path
+
+        with open(img_name, 'wb') as file:  # write into file by byte
+
+            file.write(html.content)
+
+            file.flush()
+
+        file.close()  # close file
+
+
+if __name__ == '__main__':
+    imgBaseUrl = "https://raw.githubusercontent.com/conflick0/MyPython/master/CarRecognize/img/"
+
+    for i in range(1, 10):
+        imgName = "car" + str(i)
+        imgURL = (imgBaseUrl + imgName + ".jpg")
+        downloadImg(imgName, imgURL)
+        get_license(imgName, imgURL)
+        time.sleep(1)
 
